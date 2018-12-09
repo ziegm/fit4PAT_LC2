4d148fc70cbfa84eac18faa9375b3f5700abc02f
Michaela Ziegler <michaela.ziegler@students.bfh.ch>
2018-12-09 16:33:03 +0100

    Changed navigation and page layout and elements of the sample project to fit4PATs need

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {PatientPage} from '../pages/patient/patient';
+import {AssessmentPage} from '../pages/assessment/assessment';
+import {TabsPage} from '../pages/tabs/tabs';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PatientPage,
+    AssessmentPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +24,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PatientPage,
+    AssessmentPage,
     TabsPage
   ],
   providers: [
--- src/pages/patient/patient.ts
+++ src/pages/patient/patient.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-patient',
+  templateUrl: 'patient.html'
 })
-export class ContactPage {
+export class PatientPage {
 
   constructor(public navCtrl: NavController) {
 
